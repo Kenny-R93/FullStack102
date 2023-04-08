fe2ebc586ad5382a09a6b0cd0a9450f96f7102e1
Kenny-R93 <krangelunc@gmail.com>
2023-04-07 22:56:32 -0400

    :Sadge

--- Day 25/Rock,Paper,ScissorsPt2.js
+++ Day 25/Rock,Paper,ScissorsPt2.js
@@ -78,7 +78,7 @@ function playTournament(player1, player2, player3, player4, playUntil) {        
 
 let FinalRound = playTournament(player1, player2, player3, player4)
 console.log(FinalRound.name + " is the World Champion!!!");             // **Pt2** Announce the tournament winner's name "[name] is the world champion";
-
+playTournament();
 
 
 
