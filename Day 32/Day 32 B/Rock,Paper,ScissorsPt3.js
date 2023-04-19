e9d43786de124ca37b8e5a0ee264b160f256fe2b
Kenny-R93 <krangelunc@gmail.com>
2023-04-18 22:57:31 -0400

    Finally conquered that project and got it done, yeah buddy!

--- Day 32/Day 32 B/Rock,Paper,ScissorsPt3.js
+++ Day 32/Day 32 B/Rock,Paper,ScissorsPt3.js
@@ -1,55 +1,52 @@
-const hands = ['rock', 'paper', 'scissors'];            // Define a hands array with the values 'rock', 'paper', and 'scissors';
+const hands = ['rock', 'paper', 'scissors'];
 
-function getHand() {            // Define a function called getHand() that returns a hand from the array using parseInt(Math.random()*10)%3
-    return hands[parseInt(Math.random()*10)%3];
+function getHand() {
+  // Define a function called getHand() that returns a hand from the array using parseInt(Math.random()*10)%3
+  return hands[parseInt(Math.random() * 10) % 3];
 };
 
+const userChoiceDisplay = document.getElementById("User-Hand");
+const computerChoiceDisplay = document.getElementById("Computer-Hand");
+const resultDisplay = document.getElementById("Result");
 
-const player1 = {name: "player 1", getHand: getHand()};   // Define two objects for two players. Each player has name and getHand() properties.
-const player2 = {name: "player 2", getHand: getHand()};
+function playRound(player1, player2) {
+  // Define a function called playRound() that:
+  const player1Hand = player1.getHand();
+  const player2Hand = player2.getHand();
+  let winner = null;
 
-function playRound(player1, player2) {          // Define a function called playRound() that:
-    const player1Hand = player1.getHand();          // Takes two player objects as arguments
-    const player2Hand = player2.getHand();
-    let winner = null;
-    // console.log(player1.name + " plays " + player1Hand);            // Logs the hands played
-    // console.log(player2.name + " plays " + player2Hand);            // Logs the hands played
-    if (player1Hand === player2Hand) {          // Gets hands from each
-        // console.log("It's a tie!");         // Logs the hands played and "it's a tie".
-        return winner;          // Returns the winner object (null if no winner) // Determines the winner
-    }
-    else if (player1Hand === 'rock' && player2Hand === 'paper') {           // Gets hands from each
-        winner = player2.name;        // Determines the winner
-        resultDisplay.innerHTML = winner + " is the winner!";// console.log(winner + " is the winner!");         // Logs the hands played and name of the winner.
-        return winner;          // Returns the winner object (null if no winner)
-    }
-        else if (player2Hand ==='scissors') {
-        winner = player1.name;
-        resultDisplay.innerHTML = winner + " is the winner!";// console.log(winner + " is the winner!");
-        return winner;
-        }
-    else if (player1Hand === 'paper' && player2Hand === 'rock') {
-    winner = player1.name;
-    resultDisplay.innerHTML = winner + " is the winner!";// console.log(winner + " is the winner!");
-    return winner;
-    }
-        else if ( player2Hand === 'scissors') {
-        winner = player2.name;
-        resultDisplay.innerHTML = winner + " is the winner!";// console.log(winner + " is the winner!");
-        return winner;
-        }
-    else if (player1Hand === 'scissors' && player2Hand ==='rock') {
+  if (player1Hand === player2Hand) {
+    // Gets hands from each
+    resultDisplay.innerHTML = "It's a tie!";
+    return winner; // Returns the winner object (null if no winner)
+  } else if (player1Hand === 'rock' && player2Hand === 'paper') {
     winner = player2.name;
-    resultDisplay.innerHTML = winner + " is the winner!";// console.log(winner + " is the winner!");
+    resultDisplay.innerHTML = winner + " is the winner!";
+    return winner; // Returns the winner object (null if no winner)
+  } else if (player2Hand === 'scissors') {
+    winner = player1.name;
+    resultDisplay.innerHTML = winner + " is the winner!";
     return winner;
-    }
-        else if (player1Hand === 'scissors' && player2Hand === 'paper') {
-        winner = player1.name;
-        resultDisplay.innerHTML = winner + " is the winner!";// console.log(winner + " is the winner!");
-            return winner;
-        }
-    };
+  } else if (player1Hand === 'paper' && player2Hand === 'rock') {
+    winner = player1.name;
+    resultDisplay.innerHTML = winner + " is the winner!";
+    return winner;
+  } else if (player2Hand === 'scissors') {
+    winner = player2.name;
+    resultDisplay.innerHTML = winner + " is the winner!";
+    return winner;
+  } else if (player1Hand === 'scissors' && player2Hand === 'rock') {
+    winner = player2.name;
+    resultDisplay.innerHTML = winner + " is the winner!";
+    return winner;
+  } else if (player1Hand === 'scissors' && player2Hand === 'paper') {
+    winner = player1.name;
+    resultDisplay.innerHTML = winner + " is the winner!";
+    return winner;
+  }
+};
 
+const possibleChoices = document.querySelectorAll('.button');
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
   const userChoice = e.target.id;
